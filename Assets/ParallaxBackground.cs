abf4c8e657187495941db62eb57633b865194468
sk <290548180@qq.com>
2024-11-17 13:58:37 +0800

    20241117 - 背景制作

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private GameObject camera;
+
+    [SerializeField] private float parallaxEffect;
+    
+    private float xPosition;
+    private float length;
+    
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        xPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceMoved = camera.transform.position.x * (1 - parallaxEffect);
+        float distanceToMove = camera.transform.position.x * parallaxEffect;
+        
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceMoved > xPosition + length)
+        {
+            xPosition = xPosition + length;
+        } else if (distanceMoved < xPosition - length)
+        {
+            xPosition = xPosition - length;
+        }
+    }
+}
